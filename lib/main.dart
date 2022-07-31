eb10eeb0e35be91932782c7f2e0c1f8ef860479e
Sherajul-Shakil <sherajulshakil14@cse.pstu.ac.bd>
2022-07-31 16:23:06 +0600

    Welcome Page complete

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flash_chat/screens/welcome_screen.dart';
+
+void main() => runApp(FlashChat());
+
+class FlashChat extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      initialRoute: WelcomeScreen.id,
+      routes: {
+        WelcomeScreen.id: (context) => WelcomeScreen(),
+      },
+    );
+  }
+}
